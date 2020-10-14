e087acdf18fb182d040fc5b5f3380137daf59c4b
Davis Van Sant <davisvansant@users.noreply.github.com>
2020-10-13 19:04:46 -0700

    initial front_end_server setup

--- front_end_server/src/main.rs
+++ front_end_server/src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+use actix_files::Files;
+use actix_web::middleware;
+use actix_web::App;
+use actix_web::HttpServer;
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    std::env::set_var("RUST_LOG", "actix_web=info");
+    env_logger::init();
+
+    HttpServer::new(|| {
+        App::new()
+            .wrap(middleware::Logger::default())
+            .service(Files::new("/", "./src/dist/").index_file("index.html"))
+    })
+    .bind("127.0.0.1:8080")?
+    .run()
+    .await
 }
