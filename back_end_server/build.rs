e0360faa328941e80ecd3f0f19cd38d137fa16e0
Davis Van Sant <davisvansant@users.noreply.github.com>
2020-10-16 13:25:36 -0700

    [back_end_server] initial build script for protobufs

--- back_end_server/build.rs
+++ back_end_server/build.rs
@@ -1,3 +1,7 @@
 fn main() {
-    // unimplemented!()
+    tonic_build::configure()
+        .build_client(false)
+        .out_dir("../proto/output")
+        .compile(&["../proto/hello/v1/hello.proto"], &["../proto"])
+        .unwrap();
 }
