56ce9ed5bc8cdfe9120d6d2577c95a846188b3f5
Davis Van Sant <davisvansant@users.noreply.github.com>
2020-10-22 11:53:57 -0700

    [front_end_app] initial setup of say hello

--- front_end_app/src/App.vue
+++ front_end_app/src/App.vue
@@ -4,3 +4,27 @@
     <h1>Welcome to Your Vue.js + TypeScript App</h1>
   </div>
 </template>
+
+<script>
+import { HelloServiceClient } from '../../proto/output/HelloServiceClientPb'
+import { SayHelloRequest } from '../../proto/output/hello_pb'
+
+const hello = new HelloServiceClient('http://0.0.0.0:9091', null, null)
+const request = new SayHelloRequest()
+const name = 'gRPC!'
+request.setName(name)
+
+const metadata = { 'grpc-web-text': '*' }
+hello.sayHello(request, metadata, (err, response) => {
+  if (err) {
+    console.log(err)
+  } else {
+    console.log(response.getMessage())
+  }
+})
+
+export default {
+  name: 'app'
+}
+
+</script>
